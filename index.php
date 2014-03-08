63ec33a415f2ecf242893f6f9d62204cc59e578d
Alex Willrock <willrock@211.ru>
2014-03-08 09:41:28 +0700

    init2

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<head>	
+	<title>
+	</title>
+	
+	<link rel="stylesheet" type="text/css" href="./css/MainMenyTable">
+</head>
+
+<body>
+	<section id="MainMenu">
+		<table id="MainMenuTable" border="1">
+			<tr>
+				<td colspan="2" id="Slogan">
+					1
+				</td>
+				<td colspan="2" id="About">
+					3
+				</td>
+				<td colspan="2" id="ImgContacts">
+					5
+				</td>
+			</tr>
+			
+			<tr>
+				<td id="ImgRandom">
+					1
+				</td>
+				<td>
+					2
+				</td>
+				<td rowspan="2" id="Team">
+					3
+				</td>
+				<td>
+					4
+				</td>
+				<td colspan="2" id="Contacts">
+					5
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					1
+				</td>
+				<td id="ImgTeam">
+					2
+				</td>
+				<td>
+					4
+				</td>
+				<td id="ImgNews">
+					5
+				</td>
+				<td id="News">
+					6
+				</td>
+			</tr>
+		</table>
+	</section>
+</body>
+</html>
